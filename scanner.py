2078189323e969dba11f75ee19cb8912c5464477
NosefU <nosefu@gmail.com>
2020-11-13 10:18:40 +0300

    Исправил подрезку пустых символов. Заменил подрезку пробелов на подрезку нулевых байт

--- scanner.py
+++ scanner.py
@@ -10,7 +10,7 @@ class Camera:
 
         # ip
         offset, length = int('4', 16), 15
-        self.ip = self._get_bytes(offset, length).decode("ascii").rstrip().lstrip()
+        self.ip = self._get_bytes(offset, length).decode("ascii").rstrip('\x00')
 
         # mac
         offset, length = int('54', 16), 6
@@ -19,19 +19,19 @@ class Camera:
 
         # netmask
         offset, length = int('14', 16), 15
-        self.netmask = self._get_bytes(offset, length).decode("ascii").rstrip().lstrip()
+        self.netmask = self._get_bytes(offset, length).decode("ascii").rstrip('\x00')
 
         # gateway
         offset, length = int('24', 16), 15
-        self.gateway = self._get_bytes(offset, length).decode("ascii").rstrip().lstrip()
+        self.gateway = self._get_bytes(offset, length).decode("ascii").rstrip('\x00')
 
         # dns1
         offset, length = int('34', 16), 15
-        self.dns1 = self._get_bytes(offset, length).decode("ascii").rstrip().lstrip()
+        self.dns1 = self._get_bytes(offset, length).decode("ascii").rstrip('\x00')
 
         # dns2
         offset, length = int('44', 16), 15
-        self.dns2 = self._get_bytes(offset, length).decode("ascii").rstrip().lstrip()
+        self.dns2 = self._get_bytes(offset, length).decode("ascii").rstrip('\x00')
 
         # port
         offset, length = int('5A', 16), 2
@@ -39,19 +39,19 @@ class Camera:
 
         # cloud id
         offset, length = int('5C', 16), 15
-        self.cloud_id = self._get_bytes(offset, length).decode("ascii").rstrip().lstrip()
+        self.cloud_id = self._get_bytes(offset, length).decode("ascii").rstrip('\x00')
 
         # name
         offset, length = int('7C', 16), 31
-        self.name = self._get_bytes(offset, length).decode("utf-8").rstrip().lstrip()
+        self.name = self._get_bytes(offset, length).decode("utf-8").rstrip('\x00')
 
         # firmware version
         offset, length = int('CC', 16), 15
-        self.firmware = self._get_bytes(offset, length).decode("ascii").rstrip().lstrip()
+        self.firmware = self._get_bytes(offset, length).decode("ascii").rstrip('\x00')
 
         # mode
         offset, length = int('DC', 16), 15
-        mode = self._get_bytes(offset, length).decode("ascii").rstrip().lstrip()
+        mode = self._get_bytes(offset, length).decode("ascii").rstrip('\x00')
         self.mode = mode[13]
 
     def _get_bytes(self, offset, length):
@@ -76,36 +76,37 @@ class Camera:
                 f'Cloud ID: {self.cloud_id}\n')
 
 
-# создаём сокет для отправки широковещалки
-sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sender.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-sender.bind(('', 8601))
-sender.settimeout(2)
-brcast_message = bytes.fromhex('44480101')      # пакет, который отправляет родная софтина
+if __name__ == '__main__':
+    # создаём сокет для отправки широковещалки
+    sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sender.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sender.bind(('', 8601))
+    sender.settimeout(2)
+    brcast_message = bytes.fromhex('44480101')      # пакет, который отправляет родная софтина
 
-# создаём сокет для приёма ответов
-receiver = socket.socket(type=socket.SOCK_DGRAM)
-receiver.bind(('localhost', 8600))
+    # создаём сокет для приёма ответов
+    receiver = socket.socket(type=socket.SOCK_DGRAM)
+    receiver.bind(('localhost', 8600))
 
-cctv = []
-cams_avail = False
+    cctv = []
+    cams_avail = False
 
-while True:
-    sender.sendto(brcast_message, ('255.255.255.255', 8600))    # отправляем широковещалку
-    try:
-        data, addr = sender.recvfrom(532)       # пытаемся получить от кого-нибудь ответ (размер буфера из вайршарка)
-    except socket.timeout as exc:               # если ответа не дождались,
-        if cams_avail:                          # то отчитываемся пользователю и засыпаем на 2 секунды
+    while True:
+        sender.sendto(brcast_message, ('255.255.255.255', 8600))    # отправляем широковещалку
+        try:
+            data, addr = sender.recvfrom(532)       # пытаемся получить от кого-нибудь ответ (размер буфера из вайршарка)
+        except socket.timeout as exc:               # если ответа не дождались,
+            if cams_avail:                          # то отчитываемся пользователю и засыпаем на 2 секунды
+                print('-' * 25)
+                print('No cams available')
+                cams_avail = False
+            sleep(2)
+            continue
+
+        cams_avail = True
+        camera = Camera(data)       # если ответ дождались, то разбираем пакет
+        # если такой камеры ещё не было, то выводим инфу о ней на консоль
+        if (camera.ip, camera.port) not in [(camera.ip, camera.port) for camera in cctv]:
+            cctv.append(camera)
             print('-' * 25)
-            print('No cams available')
-            cams_avail = False
-        sleep(2)
-        continue
-
-    cams_avail = True
-    camera = Camera(data)       # если ответ дождались, то разбираем пакет
-    # если такой камеры ещё не было, то выводим инфу о ней на консоль
-    if (camera.ip, camera.port) not in [(camera.ip, camera.port) for camera in cctv]:
-        cctv.append(camera)
-        print('-' * 25)
-        print(camera.full_info)
+            print(camera.full_info)
