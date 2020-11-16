f152a15cee47d6470e13527d371c2ce17052491b
NosefU <nosefu@gmail.com>
2020-11-16 09:15:20 +0300

    Добавил контекстное меню для entry

--- gui.py
+++ gui.py
@@ -4,6 +4,28 @@ from scanner import *
 import tkinter as tk
 
 
+def make_textmenu(root):
+    global the_menu
+    the_menu = tk.Menu(root, tearoff=0)
+    the_menu.add_command(label="Cut")
+    the_menu.add_command(label="Copy")
+    the_menu.add_command(label="Paste")
+    the_menu.add_separator()
+    the_menu.add_command(label="Select all")
+
+def callback_select_all(event):
+    # select text after 50ms
+    root.after(50, lambda: event.widget.select_range(0, 'end'))
+
+def show_textmenu(event):
+    e_widget = event.widget
+    the_menu.entryconfigure("Cut", command=lambda: e_widget.event_generate("<<Cut>>"))
+    the_menu.entryconfigure("Copy", command=lambda: e_widget.event_generate("<<Copy>>"))
+    the_menu.entryconfigure("Paste", command=lambda: e_widget.event_generate("<<Paste>>"))
+    the_menu.entryconfigure("Select all", command=lambda: e_widget.select_range(0, 'end'))
+    the_menu.tk.call("tk_popup", the_menu, event.x_root, event.y_root)
+
+
 class CameraWidget:
     def __init__(self, root_widget, camera):
         self.camera = camera
@@ -33,6 +55,10 @@ class CameraWidget:
 
 response = open('raw_cam_rasponse.bin', 'rb').read()
 root = tk.Tk()
+make_textmenu(root)
+root.bind_class("Entry", "<Button-3><ButtonRelease-3>", show_textmenu)
+root.bind_class("Entry", "<Control-a>", callback_select_all)
+
 camera = Camera(response)
 camera2 = deepcopy(camera)
 camera3 = deepcopy(camera)
